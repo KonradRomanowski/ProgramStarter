343169324935ad3c4d240b79db407b14af228b7c
Konrad Romanowski <k.romanowski.x@wp.pl>
2017-11-24 20:46:14 +0100

    v. 1.0.0.6
    Added BaseViewModel class

--- ViewModels/StartProgramsViewModel.cs
+++ ViewModels/StartProgramsViewModel.cs
@@ -10,32 +10,14 @@ using System.Windows;
 
 namespace ProgramStarter.ViewModels
 {
-    public class StartProgramsViewModel : INotifyPropertyChanged
+    public class StartProgramsViewModel : BaseViewModel
     {
-        private string mSeconds_To_Start;
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        private void OnPropertyChanged(string propertyName)
-        {
-            PropertyChangedEventHandler handler = PropertyChanged;
-            if (propertyName == null)
-                throw new ArgumentNullException("propertyExpression");
-
-            if (handler != null)
-            {
-                handler(this, new PropertyChangedEventArgs(propertyName));
-            }
-
-            //if (PropertyChanged != null) { PropertyChanged(this, new PropertyChangedEventArgs(propertyName)); }
-
-        }        
+        private string mSeconds_To_Start;  
 
         public string Seconds_To_Start
         {
             get
-            {
-                //if (mSeconds_To_Start == null)
-                //    return "x";
+            {                
                 return mSeconds_To_Start;
             }
 
@@ -45,8 +27,7 @@ namespace ProgramStarter.ViewModels
                     return;
 
                 mSeconds_To_Start = value;
-                OnPropertyChanged(nameof(Seconds_To_Start));
-                //PropertyChanged(this, new PropertyChangedEventArgs(nameof(Seconds_To_Start)));                                
+                OnPropertyChanged(nameof(Seconds_To_Start));                                               
             }
         }
 
