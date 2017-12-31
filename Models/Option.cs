7ec728187b0538a72fca2a1fcc129e171a6f9241
Konrad Romanowski <k.romanowski.x@wp.pl>
2017-12-31 11:18:31 +0100

    Created new class Option
    This class will represent user options, have two properties: OptionName
    and OptionValue. Two argument constructor and method ChangeOptionValue.
    This class will be used to import and export user options to
    configuration.xml file.

---
+++ Models/Option.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramStarter.Models
+{
+    public class Option
+    {
+        public string OptionName { get; private set; }
+        public string OptionValue { get; private set; }
+
+        public Option(string optionName, string optionValue)
+        {
+            OptionName = optionName;
+            OptionValue = optionValue;
+        }
+
+        public void ChangeOptionValue(string value)
+        {
+            OptionValue = value;
+        }
+    }    
+}
