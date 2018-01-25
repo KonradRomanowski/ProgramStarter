ac377c4568001d53bddf81f00322610d03e45f48
Konrad Romanowski <k.romanowski.x@wp.pl>
2018-01-25 10:51:29 +0100

    Created model of ErrorLog
    This model will be used for logging errors during starting of programs
    procedure

---
+++ Models/ErrorLog.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramStarter.Models
+{
+    /// <summary>
+    /// Error log for logging errors during starting of programs procedure
+    /// </summary>
+    public class ErrorLog
+    {
+        public DateTime DateAndTime { get; private set; }
+
+        public string ProgramName { get; private set; }
+
+        public string ProgramPath { get; private set; }
+
+        public string ErrorDescription { get; private set; }
+
+        /// <summary>
+        /// Creates a new error log for starting of programs procedure
+        /// </summary>
+        /// <param name="_dateandtime">Date and time when error occurs</param>
+        /// <param name="_programname">Program name</param>
+        /// <param name="_programpath">Path</param>
+        /// <param name="_errordescription">Error description</param>
+        public ErrorLog(DateTime _dateandtime, string _programname, string _programpath, string _errordescription)
+        {
+            DateAndTime = _dateandtime;
+            ProgramName = _programname;
+            ProgramPath = _programpath;
+            ErrorDescription = _errordescription;
+        }
+    }
+}
