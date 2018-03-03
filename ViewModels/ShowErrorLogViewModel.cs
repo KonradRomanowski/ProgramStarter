3f3376041ac2212e7ae6d79f2d3badeca140a925
Konrad Romanowski <k.romanowski.x@wp.pl>
2018-03-03 18:14:03 +0100

    Created ShowErrorLogViewModel

---
+++ ViewModels/ShowErrorLogViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ProgramStarter.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgramStarter.ViewModels
+{
+    public class ShowErrorLogViewModel : BaseViewModel
+    {
+        #region Variables Definition
+
+        /// <summary>
+        /// List with all Error Logs
+        /// </summary>
+        public ObservableCollection<ErrorLog> ErrorLogsList { get; set; }
+
+        #endregion Variables Definition
+
+        public ShowErrorLogViewModel(List<ErrorLog> _errorLogsList)
+        {
+            ErrorLogsList = new ObservableCollection<ErrorLog>();
+
+            //assing all ErrorLogs from _errorLogsList to ObservableCollection ErrorLogsList
+            foreach (ErrorLog log in _errorLogsList)
+            {
+                ErrorLogsList.Add(log);
+            }
+        }
+    }
+}
