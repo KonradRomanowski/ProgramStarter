49936aa8eb8a2bcb6dcf19b7d8be262f93b4775f
Konrad Romanowski <k.romanowski.x@wp.pl>
2018-04-22 19:30:38 +0200

    Added new RelayCommand with generic type
    RelayCommand<T>

--- Helpers/RelayCommand.cs
+++ Helpers/RelayCommand.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,9 +28,7 @@ namespace ProgramStarter.Helpers
 
             _execute = execute;
             _canExecute = canExecute;
-        }
-
-
+        }     
 
         public event EventHandler CanExecuteChanged
         {
@@ -46,5 +45,51 @@ namespace ProgramStarter.Helpers
         {
             _execute(parameter);
         }
+
+
+    }
+
+    public class RelayCommand<T> : ICommand
+    {
+
+        readonly Action<T> _execute = null;
+        readonly Predicate<T> _canExecute = null;
+               
+        public RelayCommand(Action<T> execute)
+            : this(execute, null)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new command.
+        /// </summary>
+        /// <param name="execute">The execution logic.</param>
+        /// <param name="canExecute">The execution status logic.</param>
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute)
+        {
+            if (execute == null)
+                throw new ArgumentNullException("execute");
+
+            _execute = execute;
+            _canExecute = canExecute;
+        }        
+
+        [DebuggerStepThrough]
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null ? true : _canExecute((T)parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _execute((T)parameter);
+        }
+
     }
 }
